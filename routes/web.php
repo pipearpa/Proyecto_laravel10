89ec1d8ac8475497b152c1fa0c6ab3abaee4d15d
Felipe Andres Aristizabal Padilla <faaristizabalp@peoplecontact.com.co>
2024-04-08 16:58:15 -0500

    se crea proyecto de aprendizaje en laravel version 10

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CursoController;
+
+Route::get('/', HomeController::class);
+
+Route:: controller(CursoController::class)->group(function(){
+    Route::get('cursos', 'index');
+
+    Route::get('cursos/create', 'create');
+
+    Route::get('cursos/{curso}', 'show');
+
+});
+
+
+
